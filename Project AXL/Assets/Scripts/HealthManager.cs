e2b9414b6978bd283fb8b4f44813e4e0279ca449
Jhonzasd <jhonjairotejada@gmail.com>
2023-08-27 00:22:02 -0500

    Sistema de vidas improved

--- Project AXL/Assets/Scripts/HealthManager.cs
+++ Project AXL/Assets/Scripts/HealthManager.cs
@@ -1,25 +1,69 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Schema;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class HealthManager : MonoBehaviour
 {
-    public int health = 3;
-    public Image[] hearts;
-    public Sprite fullHeart;
-    public Sprite emptyHeart;
+    public int health = 300; // Establece la cantidad de vida del jugador
+
+    public Image[] hearts; // Crea un array (lista) con los sprites de los corazones
+    public Sprite heart0; // Crea una variable que almacena el sprite del corazón lleno
+    public Sprite heart25; // Crea una variable que almacena el sprite del corazón vacío
+    public Sprite heart50;
+    public Sprite heart75;
+    public Sprite heart100;
+
+    private void FillHeart(Image heart, int fill)
+    {
+        switch (fill)
+        {
+            case 100:
+                heart.sprite = heart100;
+                break;
+            case 75:
+                heart.sprite = heart75;
+                break;
+            case 50:
+                heart.sprite = heart50;
+                break;
+            case 25:
+                heart.sprite = heart25;
+                break;
+            default:
+                heart.sprite = heart0;
+                break;
+        }
+    }
 
     // Update is called once per frame
     void Update()
     {
-        foreach(Image img in hearts)
+        // Todos los corazones inician vacíos
+        foreach (Image heart in hearts)
         {
-            img.sprite = emptyHeart;
+            FillHeart(heart, 0);
         }
-        for(int i = 0; i < health; i++)
+
+        // Determinamos cuantos corazones se llenan enteros
+        int hearts_parts = health / 25;
+        int exact_hearts = (int)(hearts_parts / 4);
+
+        // Corazón que no se llena completamente
+        int remaining = hearts_parts % 4;
+
+        // Se llenan los corazones completos
+        for (int i = 0; i < exact_hearts; i++)
         {
-            hearts[i].sprite = fullHeart;
+            FillHeart(hearts[i], 4 * 25);
         }
+
+        // Si hay un corazón que se llena parcialmente se realiza la acción
+        if (remaining > 0)
+        {
+            FillHeart(hearts[exact_hearts], remaining * 25);
+        }        
     }
 }
