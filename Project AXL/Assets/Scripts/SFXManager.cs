ad9cf51499120b6f69bc07f664a31ec1c74921eb
Jhonzasd <jhonjairotejada@gmail.com>
2023-09-29 16:53:12 -0500

    Añade SFX Manager y NPC Tienda

---
+++ Project AXL/Assets/Scripts/SFXManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SFXManager : MonoBehaviour
+{
+    public AudioSource playerHurt, playerDead, playerAttack;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
